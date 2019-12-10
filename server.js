d2b474fd4f02fb9e622b7303c074e675b63caca2
Chirag <pawarcrg@live.com>
2019-12-10 17:02:50 +0530

    index change

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/whereby'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/whereby/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/whereby/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 3000);
