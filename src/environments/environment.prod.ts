1df699e9391f1f9a502052afe7c09543fdcf4970
Chirag <pawarcrg@live.com>
2019-12-12 15:42:37 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  ApiUrl: 'http://localhost:3000/'
+  ApiUrl: ' https://wherebybackend.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  ApiUrl: 'http://localhost:3000/'
+  ApiUrl: ' https://wherebybackend.herokuapp.com/'
 };
 
 /*
